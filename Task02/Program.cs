06dae4d8f064195ab912f45dbcb188d978fc8633
NechaevMichael <nechaevmichael@mail.ru>
2022-08-31 23:14:46 +0500

    add Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число болшее, а какое меньшее.
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write($"max = {a}");
+}
+else
+{
+    Console.WriteLine($"max = {b}");
+}
