95ee008e670a836616e9b83188c8ad7d257f3709
NechaevMichael <nechaevmichael@mail.ru>
2022-09-01 21:52:42 +0500

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int maximum = a;
+
+if (maximum < b)
+{
+    maximum = b;
+
+    if (maximum < c)
+    {
+        maximum = c;
+    }
+}
+
+Console.Write(maximum);
