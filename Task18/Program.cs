856100b5d548bb28b604f94fe09b9b7c0d29d1ce
NechaevMichael <nechaevmichael@mail.ru>
2022-09-06 23:00:15 +0500

    correct Task18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -2,15 +2,15 @@
 // заданному номеру четверти, показывает диапазон
 // возможных координат точек в этой четверти (x и y).
 
-Console.WriteLine("Введите номер четверти: ");
-int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер четверти: ");
+string x = Console.ReadLine();
 
-string Quarter(int xc)
+string Quarter(string xc)
 {
-    if (xc == 1) return "x > 0, y > 0";
-    if (xc == 2) return "x < 0, y > 0";
-    if (xc == 3) return "x < 0, y < 0";
-    if (xc == 4) return "x > 0, y < 0";
+    if (xc == "1") return "x > 0, y > 0";
+    if (xc == "2") return "x < 0, y > 0";
+    if (xc == "3") return "x < 0, y < 0";
+    if (xc == "4") return "x > 0, y < 0";
     return "Вы ввели некорректное число, попробуйте ещё раз";
 }
 string result = Quarter(x);
