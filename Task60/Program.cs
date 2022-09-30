52959dc62f32da255ada3112718dc2868951b4d8
NechaevMichael <nechaevmichael@mail.ru>
2022-09-30 13:00:46 +0500

    add Task60

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -4,22 +4,47 @@
 // 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
 // 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
 
-int[,,] CreateMatrix(int rows, int columns, int size, int min, int max)
-{
-    int[,,] matrix = new int[rows, columns, size];
-    Random rnd = new Random();
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            for (int k = 0; k < matrix.GetLength(2); k++)
-            {
-                matrix[i, j, k] = rnd.Next(min, max + 1);
-            }
-        }
-    }
-    return matrix;
-}
+// int[,,] CreateMatrix(int rows, int columns, int size, int min, int max)
+// {
+//     int[,,] matrix = new int[rows, columns, size];
+//     Random rnd = new Random();
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < matrix.GetLength(2); k++)
+//             {
+//                 matrix[i, j, k] = rnd.Next(min, max + 1);
+//             }
+//         }
+//     }
+//     return matrix;
+// }
+
+int RecRandom(int num) 
+ { 
+     int n = new Random().Next(10, 100); 
+     if (num == n) return RecRandom(num); 
+     else return n; 
+ }
+
+int[,,] CreateMatrix(int rows, int col, int depth, int min, int max) 
+ { 
+     int[,,] matr = new int[rows, col, depth]; 
+     int num = new Random().Next(10, 100); 
+     for (int i = 0; i < matr.GetLength(0); i++) 
+     { 
+         for (int j = 0; j < matr.GetLength(1); j++) 
+         { 
+             for (int k = 0; k < matr.GetLength(2); k++) 
+             { 
+                 int number = RecRandom(num); 
+                 matr[i, j, k] = number; 
+             } 
+         } 
+     } 
+     return matr; 
+ }
 
 void PrintMatrix(int[,,] matrix)
 {
@@ -38,5 +63,5 @@ void PrintMatrix(int[,,] matrix)
     }
 }
 
-int[,,] result = CreateMatrix(2 ,2 ,2 ,10, 99);
+int[,,] result = CreateMatrix(2, 2, 2, 10, 99);
 PrintMatrix(result);
